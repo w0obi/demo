4e0a47876f4d66fcb307c71a6fe65157e94fa865
w0obi <123972232+w0obi@users.noreply.github.com>
2024-11-07 11:44:58 +0900

    Update WebMvcConfig.java

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://43.203.219.221:3000/")
+			.allowedOrigins("http://43.203.219.221:3000")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true)
